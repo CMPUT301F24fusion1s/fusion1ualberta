29716cca89c91de3a7bbb9f394cd0878a3dce6a1
MoTheCracker31 <113314673+MoTheCracker31@users.noreply.github.com>
2024-09-27 15:42:32 -0600

    edit shape

--- app/src/main/java/com/example/fusion1/Shape.java
+++ app/src/main/java/com/example/fusion1/Shape.java
@@ -3,9 +3,5 @@ package com.example.fusion1;
 abstract class Shape {
     int x;
     int y;
-<<<<<<< Updated upstream
-    String color = "blue";
-=======
     private String color = "blue";
->>>>>>> Stashed changes
 }
